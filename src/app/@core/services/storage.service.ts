7a55d5d13520a0f1830118670d2b3efabaa1ef42
curedas <codjocuredas@gmail.com>
2021-04-15 14:32:15 +0000

    feat(@core): adds storage service

---
+++ src/app/@core/services/storage.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  async setKeyValue(key: string, itemValue: any) {
+    return await Storage.set({
+      key,
+      value: JSON.stringify(itemValue),
+    });
+  }
+
+  async getKeyValue(key: string) {
+    const { value } = await Storage.get({ key });
+
+    return JSON.stringify(value) || null;
+  }
+
+  async removeKeyValue(key: string) {
+    return await Storage.remove({ key });
+  }
+
+  async clearKeys() {
+    return await Storage.clear();
+  }
+}
