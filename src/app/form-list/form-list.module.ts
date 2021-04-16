823287506ba63e2e9a260b5fea0799593413f968
curedas <codjocuredas@gmail.com>
2021-04-16 13:44:23 +0000

    feat: integrates form-list page

--- src/app/form-list/form-list.module.ts
+++ src/app/form-list/form-list.module.ts
@@ -7,13 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { FormListPageRoutingModule } from './form-list-routing.module';
 
 import { FormListPage } from './form-list.page';
+import { TranslateModule } from '@ngx-translate/core';
+import { NbCardModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FormListPageRoutingModule
+    FormListPageRoutingModule,
+    TranslateModule.forChild(),
+    NbCardModule
   ],
   declarations: [FormListPage]
 })
