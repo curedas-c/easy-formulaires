f3a623227ba7535826641a57c9ec43892ab808a5
curedas <codjocuredas@gmail.com>
2021-04-16 12:51:20 +0000

    feat: integrates main sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,6 @@ import { LanguageService } from './@core/services/language.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/form-list', icon: 'mail' },
-    { title: 'Create', url: '/form-create', icon: 'paper-plane' }
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor(private language: LanguageService) {
     this.language.initLanguage();
   }
