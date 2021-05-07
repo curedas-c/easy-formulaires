64bbb7bf494fc0dd769ebc53ee5e6e2fc81a3295
curedas <codjocuredas@gmail.com>
2021-05-07 19:15:14 +0000

    fix: fix back button always closing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { LanguageService } from './@core/services/language.service';
 import { version } from '../../package.json';
 import { FormStateService } from './@core/services/form-state.service';
+import { App } from '@capacitor/app';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,7 +13,21 @@ export class AppComponent {
   public version: string = version;
   public toggleChecked = false;
 
-  constructor(private language: LanguageService, private formState: FormStateService) {
+  constructor(
+    private language: LanguageService,
+    private formState: FormStateService,
+    private location: Location
+  ) {
+    App.addListener('backButton', () => {
+      const IS_MAIN_ROUTE = this.location.isCurrentPathEqualTo('/form-list');
+      if (!IS_MAIN_ROUTE) {
+        this.location.back();
+      } else {
+        App.exitApp();
+      }
+    });
+
+    // translation initialisation
     this.language.initLanguage().then((langCode) => {
       if (langCode === 'fr') {
         this.toggleChecked = true;
